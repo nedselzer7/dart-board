9189cc4127181f894d1e721a3f76b9203386ce2f
Ned Selzer <nedselzer@Neds-MacBook-Air.local>
2022-02-03 18:27:18 -0500

    add player_profiles table

--- db/v1_initial_setup.sql
+++ db/v1_initial_setup.sql
@@ -12,3 +12,11 @@ create table dartboard.winners_corner
 
 insert into dartboard.winners_corner(player, win_count, game_type, bb_y_n)
   values ('Test Player', 0, 'Cricket', 'Y');
+
+create table dartboard.player_profiles
+(
+  player_name varchar(100) not null,
+  description varchar (350),
+  date_added timestamp default current_timestamp,
+  primary key (player_name)
+)
